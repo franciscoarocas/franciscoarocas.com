72ae126f69d4ec50e4c8b39c5750eaac35fd61e4
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2022-12-18 16:11:20 +0000

    Improved lang button and added cookies

--- src/components/topNav.tsx
+++ src/components/topNav.tsx
@@ -1,9 +1,12 @@
 
 import * as React from "react"
 
+import { useCookies } from 'react-cookie';
+
 import { FormattedMessage } from 'react-intl';
 
 import ESIcon from '../images/es.png'
+import ENIcon from '../images/gb.png'
 
 const links = [
   {
@@ -26,7 +29,42 @@ const links = [
 
 const TopNav = ({currentPage}) => {
 
+  const language = navigator.language.split(/[-_]/)[0] ? 'es' : 'en';
   const currentPageURL = currentPage.length > 1 ? currentPage.substring(0, currentPage.length - 1) : currentPage;
+
+  const [cookies, setCookie] = useCookies(['lang']);
+
+
+  const generateLangIcon = () => {
+
+    const currentLang = !cookies['lang'] ? language : cookies['lang'];
+
+    if (currentLang === 'es') {
+      return (
+        <>
+          <img src={ESIcon}/> Español
+        </>
+      )
+    } else {
+      return (
+        <>
+          <img src={ENIcon}/> English
+        </>
+      );
+    }
+  }
+
+
+  const changeLang = (_ : React.MouseEvent<HTMLInputElement>) => {
+
+    const currentLang = !cookies['lang'] ? language : cookies['lang'];
+
+    console.log(currentLang)
+
+    setCookie('lang', currentLang === 'es' ? 'en' : 'es');
+
+  }
+
 
   return (
     <nav className="topNav">
@@ -36,7 +74,7 @@ const TopNav = ({currentPage}) => {
         })
       }
       {/*<li>Blog</li>*/}
-      {<li className="langButton" key={links.length}><img src={ESIcon}/> Español</li>}
+      {<li onClick={changeLang} className="langButton" key={links.length}>{generateLangIcon()}</li>}
     </nav>
   )
 
