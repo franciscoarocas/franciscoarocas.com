14442e660f9098e14e28115d974411abcaae8609
franciscoarocas <54645175+franciscoarocas@users.noreply.github.com>
2022-12-19 20:32:04 +0000

    Improved cookie message

--- src/components/cookiesMessage.tsx
+++ src/components/cookiesMessage.tsx
@@ -2,15 +2,24 @@ import * as React from "react"
 
 import { BiCookie } from 'react-icons/bi';
 
+import { RxCross2 } from 'react-icons/rx';
+
 const CookiesMessage = () => {
+
+  const [showMessage, setShowMessage] = React.useState(false);
 
   return (
     <div className="cookieMessageDiv">
-      <div className="cookieMessage">
-        En esta web solo utilizamos cookies para el cambio de idiomas
+      <div className={`cookieMessage ${showMessage ? "" : "hide"}`}>
+        <p>En esta web solo utilizamos cookies para el cambio de idiomas.</p>
+        <p>Al cambiar el idioma, se almacena este en una cookie.</p>
       </div>
-      <div className="cookieMessageIcon">
-        <BiCookie fill="white" size={48}/>
+      <div className="cookieMessageIcon" onClick={() => { setShowMessage(!showMessage) }}>
+        {
+          !showMessage ?
+          <BiCookie fill="white" size={48}/> : 
+          <RxCross2 color="white" size={48}/>
+        }
       </div>
     </div>
   );
